91b56e159cc34c8d8d318b22e7fa0af230e5213a
Tushar Verma <110741618+1tushr@users.noreply.github.com>
2023-03-16 19:59:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-let yourArray = ["hello", 42, true, null, "world"]; 
+let yourArray = [
+  [
+    {
+      one: 1,
+      two: 2
+    },
+    {
+      three: 3,
+      four: 4
+    }
+  ],
+  [
+    {
+      a: "a",
+      b: "b"
+    },
+    {
+      c: "c",
+      d: "d"
+    }
+  ],
+  true,
+  42,
+  "hello world"
+];
